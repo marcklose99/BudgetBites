7878eccdef8ba7229c7979c1b475b90368383936
Marc Klose <marcklose99@gmail.com>
2023-05-23 15:32:36 +0200

    Initial commit

---
+++ src/main/java/com/budgetbites/budgetbitesapi/BudgetbitesApiApplication.java
@@ -0,0 +1,13 @@
+package com.budgetbites.budgetbitesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetbitesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetbitesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budgetbites/budgetbitesapi/BudgetbitesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetbites.budgetbitesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetbitesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
