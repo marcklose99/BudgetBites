edffd47331ce9adc7c44bc767a196c115b6b3f55
Marc Klose <marcklose99@gmail.com>
2023-05-23 16:10:57 +0200

    [ADD] Endpoints for CRUD operations

---
+++ src/main/java/com/budgetbites/budgetbitesapi/controller/RecipeController.java
@@ -0,0 +1,40 @@
+package com.budgetbites.budgetbitesapi.controller;
+
+import com.budgetbites.budgetbitesapi.models.Recipe;
+import com.budgetbites.budgetbitesapi.services.RecipeServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class RecipeController {
+    private final RecipeServiceImpl recipeService;
+
+    @GetMapping("/recipes")
+    public ResponseEntity<List<Recipe>> getAllRecipes() {
+        return recipeService.getAllRecipes();
+    }
+
+    @GetMapping("/recipes/{id}")
+    public ResponseEntity<Recipe> getSingleRecipe(@PathVariable Long id) {
+        return recipeService.getRecipeById(id);
+    }
+
+    @PostMapping("/recipes")
+    public ResponseEntity<Recipe> createRecipe(@RequestBody Recipe recipe) {
+        return recipeService.createRecipe(recipe);
+    }
+
+    @PutMapping("/recipes/{id}")
+    public ResponseEntity<Recipe> updateRecipe(@PathVariable Long id, @RequestBody Recipe recipe) {
+        return recipeService.updateRecipe(id, recipe);
+    }
+
+    @DeleteMapping("/recipes/{id}")
+    public ResponseEntity<Recipe> deleteRecipe(@PathVariable Long id) {
+        return recipeService.deleteRecipe(id);
+    }
+}
