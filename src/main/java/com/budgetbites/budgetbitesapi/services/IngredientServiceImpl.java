aceceea05f23041cd9d79054037d7c74a4a52514
Marc Klose <marcklose99@gmail.com>
2023-05-24 10:32:27 +0200

    [ADD] Implemented business logic to retrieve data and mapping to ingredient identity

--- src/main/java/com/budgetbites/budgetbitesapi/services/IngredientServiceImpl.java
+++ src/main/java/com/budgetbites/budgetbitesapi/services/IngredientServiceImpl.java
@@ -3,11 +3,19 @@ package com.budgetbites.budgetbitesapi.services;
 import com.budgetbites.budgetbitesapi.exceptions.IngredientNotFoundException;
 import com.budgetbites.budgetbitesapi.models.Ingredient;
 import com.budgetbites.budgetbitesapi.repository.IngredientsRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -16,27 +24,74 @@ public class IngredientServiceImpl implements IngredientService {
 
     private final IngredientsRepository ingredientsRepository;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public ResponseEntity<List<Ingredient>> getAllIngredients() {
-        List<Ingredient> ingredients = ingredientsRepository.findAll();
-        return new ResponseEntity<>(ingredients, HttpStatus.OK);
+    public List<Ingredient> getMatchingIngredients(String title) {
+        return ingredientsRepository.findByTitle(title);
     }
 
     @Override
-    public ResponseEntity<Ingredient> getIngredientById(Long id) {
-        Ingredient ingredient = ingredientsRepository.findById(id)
+    public Ingredient getIngredientById(Long id) {
+        return ingredientsRepository.findById(id)
                 .orElseThrow(() -> new IngredientNotFoundException(id));
-        return new ResponseEntity<>(ingredient, HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<Ingredient> createIngredient(Ingredient ingredient) {
-        try {
-            ingredientsRepository.save(ingredient);
-            return new ResponseEntity<>(ingredient, HttpStatus.CREATED);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public List<Ingredient> fetchIngredients() throws IOException, InterruptedException {
+        int fetchedResultsCount = 0;
+        List<Ingredient> allResults = new ArrayList<>();
+
+        JsonNode responseAsJson = getResponseFromWebsite(fetchedResultsCount);
+        int totalOfferResults = responseAsJson.get("totalResults").intValue();
+
+        JsonNode resultsAsJson;
+
+        while (fetchedResultsCount < totalOfferResults) {
+            resultsAsJson = responseAsJson.get("results");
+            fetchedResultsCount += resultsAsJson.size();
+            List<Ingredient> fetchedResults = Arrays.stream(objectMapper.readValue(String.valueOf(resultsAsJson), Ingredient[].class)).toList();
+            allResults.addAll(fetchedResults);
+            responseAsJson = getResponseFromWebsite(fetchedResultsCount);
         }
-        return null;
+
+        for (Ingredient ingredient : allResults) {
+            if (ingredientsRepository.findById(ingredient.getId()).isEmpty()) {
+                ingredientsRepository.save(ingredient);
+            } else {
+                ingredient = ingredientsRepository.findById(ingredient.getId()).orElseThrow();
+                ingredientsRepository.saveAndFlush(ingredient);
+            }
+        }
+        return allResults;
     }
+
+    @Override
+    public JsonNode getResponseFromWebsite(int fetchedResultsCount) throws IOException, InterruptedException {
+
+        String url = String.format("https://api.marktguru.de/api/v1/industries/supermaerkte/offers?as=mobile&limit=512&offset=%s&zipCode=44787", fetchedResultsCount);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request;
+        try {
+            request = HttpRequest.newBuilder(new URI(url))
+                    .headers(
+                            "Content-Type", "application/json",
+                            "X-ApiKey", "8Kk+pmbf7TgJ9nVj2cXeA7P5zBGv8iuutVVMRfOfvNE=",
+                            "X-ClientKey", "Qs5EM9EteUFUcv6Wx/+omXnHmVMn1PH789u392ewfNE="
+                    )
+                    .build();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return objectMapper.readTree(response.body());
+    }
+
+    @Override
+    public void createIngredient(Ingredient ingredient) {
+        ingredientsRepository.save(ingredient);
+    }
+
 }
