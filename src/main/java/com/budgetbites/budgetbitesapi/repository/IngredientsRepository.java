e7d61c35333ac4790ddd4c571a30c23458bdd654
Marc Klose <marcklose99@gmail.com>
2023-05-24 10:33:09 +0200

    [ADD] Query to retrieve data from db by title

--- src/main/java/com/budgetbites/budgetbitesapi/repository/IngredientsRepository.java
+++ src/main/java/com/budgetbites/budgetbitesapi/repository/IngredientsRepository.java
@@ -2,8 +2,13 @@ package com.budgetbites.budgetbitesapi.repository;
 
 import com.budgetbites.budgetbitesapi.models.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IngredientsRepository extends JpaRepository<Ingredient, Long> {
+    @Query(value = "SELECT * FROM ingredient i WHERE i.title LIKE CONCAT('%', ?1, '%')", nativeQuery = true)
+    List<Ingredient> findByTitle(String title);
 }
