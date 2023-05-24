404430a08e7f049758e2bd01ecb9dfa1e4c54116
Marc Klose <marcklose99@gmail.com>
2023-05-24 10:27:11 +0200

    [REFACTOR] Removed endpoints getAll, getSingle and create and added method to get matching results to a title

--- src/main/java/com/budgetbites/budgetbitesapi/controller/IngredientController.java
+++ src/main/java/com/budgetbites/budgetbitesapi/controller/IngredientController.java
@@ -1,32 +1,25 @@
 package com.budgetbites.budgetbitesapi.controller;
 
 import com.budgetbites.budgetbitesapi.models.Ingredient;
-import com.budgetbites.budgetbitesapi.services.IngredientServiceImpl;
+import com.budgetbites.budgetbitesapi.services.IngredientService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class IngredientController {
-    private final IngredientServiceImpl ingredientService;
 
-    @GetMapping("/ingredients")
-    public ResponseEntity<List<Ingredient>> getAllIngredients() {
-        return ingredientService.getAllIngredients();
-    }
+    private final IngredientService ingredientService;
 
-    @GetMapping("/ingredients/{id}")
-    public ResponseEntity<Ingredient> getSingleIngredient(@PathVariable Long id) {
-        return ingredientService.getIngredientById(id);
-    }
-
-    @PostMapping(
-            value = "/ingredients",
-            consumes = "application/json")
-    public ResponseEntity<Ingredient> createIngredient(@RequestBody Ingredient ingredient) {
-        return ingredientService.createIngredient(ingredient);
+    @GetMapping("/ingredients/{title}")
+    public ResponseEntity<List<Ingredient>> getMatchingIngredients(@PathVariable String title) {
+        List<Ingredient> matchingIngredients = ingredientService.getMatchingIngredients(title);
+        return new ResponseEntity<>(matchingIngredients, HttpStatus.OK);
     }
 }
