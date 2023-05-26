f6b46a4e0c5b4a701458d8dc2861720190d38ebf
Marc Klose <marcklose99@gmail.com>
2023-05-26 13:06:39 +0200

    [ADD] Method findMinDate to find the smallest valid_to date of all results and findByDate to get all results with matching valid_to date

--- src/main/java/com/budgetbites/budgetbitesapi/repository/IngredientsRepository.java
+++ src/main/java/com/budgetbites/budgetbitesapi/repository/IngredientsRepository.java
@@ -5,10 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface IngredientsRepository extends JpaRepository<Ingredient, Long> {
     @Query(value = "SELECT * FROM ingredient i WHERE i.title LIKE CONCAT('%', ?1, '%')", nativeQuery = true)
     List<Ingredient> findByTitle(String title);
+
+    @Query(value = "SELECT * FROM ingredient i WHERE i.valid_to LIKE CONCAT('%', ?1, '%')", nativeQuery = true)
+    List<Ingredient> findByDate(Date executionDate);
+
+    @Query(value = "SELECT * FROM ingredient WHERE is_valid = true AND valid_to = (SELECT MIN(valid_to) FROM ingredient WHERE is_valid = true) LIMIT 1", nativeQuery = true)
+    Ingredient findMinDate();
 }
