924e23ba55b6a824307aad8e94d422c256d7cd75
Marc Klose <marcklose99@gmail.com>
2023-06-08 10:52:31 +0200

    [REFACTOR] Added cross origin annotation

--- src/main/java/com/budgetbites/budgetbitesapi/controller/IngredientController.java
+++ src/main/java/com/budgetbites/budgetbitesapi/controller/IngredientController.java
@@ -5,6 +5,7 @@ import com.budgetbites.budgetbitesapi.services.IngredientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,13 @@ public class IngredientController {
 
     private final IngredientService ingredientService;
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/ingredients/{title}")
     public ResponseEntity<List<Ingredient>> getMatchingIngredients(@PathVariable String title) {
-        List<Ingredient> matchingIngredients = ingredientService.getMatchingIngredients(title);
-        return new ResponseEntity<>(matchingIngredients, HttpStatus.OK);
+        if (!title.isEmpty() && title.length() != 1) {
+            List<Ingredient> matchingIngredients = ingredientService.getMatchingIngredients(title);
+            return new ResponseEntity<>(matchingIngredients, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
