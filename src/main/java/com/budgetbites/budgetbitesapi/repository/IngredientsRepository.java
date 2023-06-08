1f1ba79b689a69ba2ab8e48d2bd25da775d51fbd
Marc Klose <marcklose99@gmail.com>
2023-06-08 10:54:51 +0200

    [REFACTOR] Changed method parameter of findByTitle

--- src/main/java/com/budgetbites/budgetbitesapi/repository/IngredientsRepository.java
+++ src/main/java/com/budgetbites/budgetbitesapi/repository/IngredientsRepository.java
@@ -3,6 +3,7 @@ package com.budgetbites.budgetbitesapi.repository;
 import com.budgetbites.budgetbitesapi.models.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -10,8 +11,8 @@ import java.util.List;
 
 @Repository
 public interface IngredientsRepository extends JpaRepository<Ingredient, Long> {
-    @Query(value = "SELECT * FROM ingredient i WHERE i.title LIKE CONCAT('%', ?1, '%')", nativeQuery = true)
-    List<Ingredient> findByTitle(String title);
+    @Query(value = "SELECT * FROM ingredient WHERE title LIKE %:title%", nativeQuery = true)
+    List<Ingredient> findByTitle(@Param("title") String title);
 
     @Query(value = "SELECT * FROM ingredient i WHERE i.valid_to LIKE CONCAT('%', ?1, '%')", nativeQuery = true)
     List<Ingredient> findByDate(Date executionDate);
