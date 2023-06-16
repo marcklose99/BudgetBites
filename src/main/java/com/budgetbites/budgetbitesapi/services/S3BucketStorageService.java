45f3287359eb462631c2e9cc79cea1ebcef22567
Marc Klose <marcklose99@gmail.com>
2023-06-16 09:33:25 +0200

    [ADD] Implementation of upload and download an image

---
+++ src/main/java/com/budgetbites/budgetbitesapi/services/S3BucketStorageService.java
@@ -0,0 +1,68 @@
+package com.budgetbites.budgetbitesapi.services;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3BucketStorageService {
+
+    Logger logger = LoggerFactory.getLogger(S3BucketStorageService.class);
+
+    private final AmazonS3 amazonS3Client;
+
+    @Value("${aws.s3.bucket.name}")
+    private String bucketName;
+
+    /**
+     * Upload file into AWS S3
+     *
+     * @param keyName
+     * @param file
+     * @return String
+     */
+    public String uploadFile(String keyName, MultipartFile file) {
+        try {
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+            amazonS3Client.putObject(bucketName, keyName, file.getInputStream(), metadata);
+            return "File uploaded: " + keyName;
+        } catch (IOException ioe) {
+            logger.error("IOException: " + ioe.getMessage());
+        } catch (AmazonServiceException serviceException) {
+            logger.info("AmazonServiceException: "+ serviceException.getMessage());
+            throw serviceException;
+        } catch (AmazonClientException clientException) {
+            logger.info("AmazonClientException Message: " + clientException.getMessage());
+            throw clientException;
+        }
+        return "File not uploaded: " + keyName;
+    }
+
+
+    public String download(String keyName) {
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest(bucketName, keyName)
+                        .withMethod(HttpMethod.GET);
+       return amazonS3Client.generatePresignedUrl(generatePresignedUrlRequest).toString();
+    }
+    public String generateFileName(MultipartFile file) {
+        return new Date().getTime() + "-" + file.getOriginalFilename().replace(" ", "_");
+    }
+}
