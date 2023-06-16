6aed987735d54e9a667201081e592968c9852be0
Marc Klose <marcklose99@gmail.com>
2023-06-16 09:36:42 +0200

    [ADD] Endpoints to retrieve image url for a given id and changed mediatype of createRecipe method

--- src/main/java/com/budgetbites/budgetbitesapi/controller/RecipeController.java
+++ src/main/java/com/budgetbites/budgetbitesapi/controller/RecipeController.java
@@ -1,10 +1,13 @@
 package com.budgetbites.budgetbitesapi.controller;
 
 import com.budgetbites.budgetbitesapi.models.Recipe;
+import com.budgetbites.budgetbitesapi.models.RecipeDTO;
 import com.budgetbites.budgetbitesapi.services.RecipeServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -24,19 +27,18 @@ public class RecipeController {
         return recipeService.getRecipeById(id);
     }
 
-    @PostMapping(
-            value = "/recipes",
-            consumes = "application/json")
-    public ResponseEntity<Recipe> createRecipe(@RequestBody Recipe recipe) {
-        if(recipe.getId() == null) {
-            return recipeService.createRecipe(recipe);
-        } else {
-            return recipeService.updateRecipe(recipe.getId(), recipe);
-        }
-
+    @PostMapping(path = "/recipes", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Recipe> createRecipe(@RequestPart RecipeDTO recipe, @RequestPart MultipartFile file) {
+        return recipeService.createRecipe(recipe, file);
     }
+
     @DeleteMapping("/recipes/{id}")
     public void deleteRecipe(@PathVariable Long id) {
         recipeService.deleteRecipe(id);
+    }
+
+    @GetMapping("/recipes/{id}/image")
+    public String getUrl(@PathVariable Long id) {
+        return recipeService.getUrl(id);
     }
 }
