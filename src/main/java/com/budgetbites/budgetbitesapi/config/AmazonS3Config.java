73e89af70922c256996ebf1a6a22dc2dacbba38c
Marc Klose <marcklose99@gmail.com>
2023-06-27 10:39:01 +0200

    [REFACTOR] FetcherService now FetchService and renamed application properties

--- src/main/java/com/budgetbites/budgetbitesapi/config/AmazonS3Config.java
+++ src/main/java/com/budgetbites/budgetbitesapi/config/AmazonS3Config.java
@@ -1,6 +1,5 @@
 package com.budgetbites.budgetbitesapi.config;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
@@ -12,19 +11,17 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonS3Config {
+    @Value("${aws.access.key.id}")
+    private String awsId;
 
-        @Value("${aws.access.key.id}")
-        private String awsId;
+    @Value("${aws.secret.access.key}")
+    private String awsKey;
 
-        @Value("${aws.secret.access.key}")
-        private String awsKey;
-
-        @Value("${aws.s3.region}")
-        private String region;
+    @Value("eu-central-1")
+    private String region;
 
     @Bean
     public AmazonS3 s3client() {
-
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsId, awsKey);
         return AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.fromName(region))
