7e4da5bc54faf47ad26b44db713dee923b4e6a6c
Marc Klose <marcklose99@gmail.com>
2023-06-28 13:15:16 +0200

    [REFACTOR] Renamed methods and added logic of creating new address objects if no object is associated with a given postal code

--- src/main/java/com/budgetbites/budgetbitesapi/services/AddressService.java
+++ src/main/java/com/budgetbites/budgetbitesapi/services/AddressService.java
@@ -14,14 +14,24 @@ import java.util.Optional;
 public class AddressService {
 
     private final AddressRepository addressRepository;
-    public boolean isRecentlyFetched(long postalCode) {
+    public boolean isPostalCodeActive(long postalCode) {
         Optional<Address> address = addressRepository.findById(postalCode);
-        return address.isPresent() && !addressExpired(address.get());
+        if(address.isEmpty()) {
+            addressRepository.save(new Address(postalCode));
+            return false;
+        }
+        return isFetchDateValid(address.get());
     }
 
-    private boolean addressExpired(Address address) {
-        LocalDate fiveDaysAgo = LocalDate.now().minus(5, ChronoUnit.DAYS);
-        return address.getLatestFetchDate().isAfter(fiveDaysAgo);
+    private boolean isFetchDateValid(Address address) {
+        LocalDate now = LocalDate.now();
+        LocalDate fiveDaysAgo = now.minus(5, ChronoUnit.DAYS);
+        if(address.getLatestFetchDate().isBefore(fiveDaysAgo)) {
+            address.setLatestFetch(now);
+            addressRepository.save(address);
+            return false;
+        }
+        return true;
     }
 
 }
