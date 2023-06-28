557e1106a8f09e559de14d89e25390a37b577741
Marc Klose <marcklose99@gmail.com>
2023-06-28 13:13:05 +0200

    [ADD] Constructor to init id and latestFetch date

--- src/main/java/com/budgetbites/budgetbitesapi/models/Address.java
+++ src/main/java/com/budgetbites/budgetbitesapi/models/Address.java
@@ -14,8 +14,24 @@ public class Address {
 
     private LocalDate latestFetch;
 
+
+    public Address(long id) {
+        this.id = id;
+        this.latestFetch = LocalDate.now();
+    }
+
+    public Address() {
+    }
+
     public LocalDate getLatestFetchDate() {
         return this.latestFetch;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setLatestFetch(LocalDate latestFetch) {
+        this.latestFetch = latestFetch;
+    }
 }
