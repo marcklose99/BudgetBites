65569e326ec73aa95d4723cf3c317da7479edaad
Marc Klose <marcklose99@gmail.com>
2023-06-28 13:16:52 +0200

    [ADD] AddressService checks if ingredients has been fetched recently

--- src/main/java/com/budgetbites/budgetbitesapi/controller/IngredientController.java
+++ src/main/java/com/budgetbites/budgetbitesapi/controller/IngredientController.java
@@ -1,11 +1,15 @@
 package com.budgetbites.budgetbitesapi.controller;
 
 import com.budgetbites.budgetbitesapi.models.Ingredient;
+import com.budgetbites.budgetbitesapi.services.AddressService;
 import com.budgetbites.budgetbitesapi.services.IngredientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -15,17 +19,20 @@ import java.util.List;
 public class IngredientController {
 
     private final IngredientService ingredientService;
+    private final AddressService addressService;
 
     @GetMapping("/ingredients/fetch/{postalCode}")
     public void fetchIngredients(@PathVariable int postalCode) {
-        ingredientService.create(postalCode);
+        if(!addressService.isPostalCodeActive(postalCode)) {
+            ingredientService.create(postalCode);
+        }
     }
 
 
     @GetMapping({"/ingredients/{title}", "/ingredients/{title}/{filter}"})
     public ResponseEntity<List<Ingredient>> getMatchingIngredients(@PathVariable String title,
                                                                    @PathVariable(required = false) String filter) {
-        if (!title.isEmpty() && title.length() > 1) {
+        if (title.length() > 1) {
             if(filter != null) {
                 List<Ingredient> matchingIngredients = ingredientService.getMatchingIngredients(title, filter.toLowerCase());
                 return new ResponseEntity<>(matchingIngredients, HttpStatus.OK);
